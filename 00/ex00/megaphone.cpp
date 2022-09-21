4f02df8dada9c898670a7b82ae16e743d5e3d15e
Leo-Gyger <gygerleo@proton.me>
2022-09-21 12:27:35 +0200

    removed using namespace, awful practice

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -12,23 +12,22 @@
 
 #include	<iostream>
 
-using namespace std;
 
 int	main(int ac, char **av)
 {
-	locale loc;
+	std::locale loc;
 
 	// For all arguments:
 	//   upper case the letters
 	//   print the string
 	for (int i = 0; ++i < ac;)
 	{
-		use_facet< ctype<char> >(loc).toupper ( av[i], av[i] +sizeof(av[i]) );
-		cout << av[i];
+		std::use_facet<std::ctype<char> >(loc).toupper ( av[i], av[i] +sizeof(av[i]) );
+		std::cout << av[i];
 	}
 
 	// Newline
-	cout << endl;
+	std::cout << std::endl;
 
 	return (0);
 }
