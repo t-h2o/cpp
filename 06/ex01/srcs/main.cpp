001f0b0a373a83d051948d961b584bbb39ca6fa4
theo <grivel.theo@protonmail.com>
2022-12-08 23:02:34 +0100

    wip

--- 06/ex01/srcs/main.cpp
+++ 06/ex01/srcs/main.cpp
@@ -1,10 +1,48 @@
 #include	<iostream>
+#include	<cstdint>
+
+typedef struct	s_data
+{
+	std::string	name;
+	int			age;
+} t_data;
+
+t_data	*new_data(void)
+{
+	return new t_data;
+}
+
+static std::uintptr_t	serialize(t_data *dataptr)
+{
+	std::uintptr_t	rawptr;
+
+	rawptr = reinterpret_cast<uintptr_t>(dataptr);
+	return rawptr;
+}
 
 int	main(void)
 {
+	t_data		*dataptr;
+	std::uintptr_t	rawraw;
+
 	std::cout
 		<< "Module 06: Exercice 01"
 		<< std::endl;
+
+	dataptr = new_data();
+	rawraw = serialize(dataptr);
+
+	std::cout
+		<< "data pointer: "
+		<< dataptr
+		<< std::endl;
+
+	std::cout
+		<< " raw pointer: "
+		<< rawraw
+		<< std::endl;
+
+	delete dataptr;
 
 	return 0;
 }
