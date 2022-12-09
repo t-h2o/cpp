1a35b2969be3ca8ef17a08e1f108a3f70f440616
|°_°| <grivel.theo@protonmail.com>
2022-12-09 01:56:44 +0100

    Create class MyIter

--- 07/ex01/srcs/main.cpp
+++ 07/ex01/srcs/main.cpp
@@ -1,10 +1,33 @@
 #include	<iostream>
 
+class MyIter
+{
+	public:
+		template<typename T>
+		static void	iter(T const & value)
+		{
+			std::cout << "Iter function called: "
+				<< value
+				<< std::endl;
+		}
+};
+
+static void	test(void)
+{
+	std::string	value;
+
+	value = "my value";
+
+	MyIter::iter(value);
+}
+
 int	main(void)
 {
 	std::cout
 		<< "Module 07 : Exercice 01 : Iter"
 		<< std::endl;
+
+	test();
 
 	return 0;
 }
