ef2733c445ffbcf5a01465c28f214af7c4bf9f91
|°_°| <grivel.theo@protonmail.com>
2022-12-09 02:19:23 +0100

    iter takes table

--- 07/ex01/srcs/main.cpp
+++ 07/ex01/srcs/main.cpp
@@ -7,20 +7,32 @@ class MyIter
 {
 	public:
 		template<typename T>
-		static void	iter(T const & value)
+		static void	iter(T const * values, unsigned int size)
 		{
 			std::cout << "Iter function called: "
-				<< value
+				<< values
+				<< ", "
+				<< size
 				<< std::endl;
+			for (unsigned int i = 0; i < size; i++)
+			{
+				std::cout
+					<< "\t"
+					<< i
+					<< " : "
+					<< values[i]
+					<< std::endl;
+			}
 		}
 };
 
 static void	test(void)
 {
-	section("Iter with a string"); NEWLINE; NEWLINE;
-	std::string	value;
-	value = "my value";
-	MyIter::iter(value);
+	section("Iter into a string table"); NEWLINE; NEWLINE;
+	std::string	values[2];
+	values[0] = "my first value";
+	values[1] = "my seconde value";
+	MyIter::iter(values, 2);
 }
 
 int	main(void)
