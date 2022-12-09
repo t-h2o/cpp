290bfdafa20594e0d9939914b5a7c7d8e428e2b7
|°_°| <grivel.theo@protonmail.com>
2022-12-09 01:33:09 +0100

    add subject test

--- 07/ex00/srcs/main.cpp
+++ 07/ex00/srcs/main.cpp
@@ -42,13 +42,44 @@ static void	test(void)
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 }
 
+static void	test_subject(void)
+{
+	int	a = 2;
+	int	b = 3;
+	Mytemplates::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << Mytemplates::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << Mytemplates::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	Mytemplates::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << Mytemplates::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << Mytemplates::max( c, d ) << std::endl;
+}
+
 int	main(void)
 {
 	std::cout
 		<< "Module 07 : Exercice 00"
 		<< std::endl;
 
+	std::cout
+		<< "my tests:"
+		<< std::endl
+		<< std::endl;
 	test();
+
+	std::cout
+		<< std::endl
+		<< std::endl;
+
+	std::cout
+		<< "Subject tests:"
+		<< std::endl
+		<< std::endl;
+	test_subject();
 
 	return 0;
 }
