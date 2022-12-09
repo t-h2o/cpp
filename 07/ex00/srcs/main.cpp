cfbfc3e789e2a013765e01c5a6b42361611d237a
|°_°| <grivel.theo@protonmail.com>
2022-12-09 01:16:24 +0100

    create my first template

--- 07/ex00/srcs/main.cpp
+++ 07/ex00/srcs/main.cpp
@@ -1,10 +1,29 @@
 #include	<iostream>
 
+template<typename TYPE>
+TYPE const &	max(TYPE const & x, TYPE const & y)
+{
+	if (x >= y)
+		return x;
+	return y;
+}
+
+static void	test(void)
+{
+	int const	a = 2;
+	int const	b = 3;
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "max( a, b ) = " << max<int>( a, b ) << std::endl;
+}
+
 int	main(void)
 {
 	std::cout
 		<< "Module 07 : Exercice 00"
 		<< std::endl;
+
+	test();
 
 	return 0;
 }
