a34bb890a0aa99e76723e1d9bb3ba30415c39885
|°_°| <grivel.theo@protonmail.com>
2022-12-09 01:28:55 +0100

    create swap

--- 07/ex00/srcs/main.cpp
+++ 07/ex00/srcs/main.cpp
@@ -18,16 +18,28 @@ class	Mytemplates
 				return x;
 			return y;
 		}
+
+		template<typename TYPE>
+		static void	swap(TYPE & x, TYPE & y)
+		{
+			TYPE tmp;
+
+			tmp	= x;
+			x = y;
+			y = tmp;
+		}
 };
 
 static void	test(void)
 {
-	int const	a = 2;
-	int const	b = 3;
+	int a = 2;
+	int b = 3;
 
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "max( a, b ) = " << Mytemplates::max<int>( a, b ) << std::endl;
 	std::cout << "min( a, b ) = " << Mytemplates::min<int>( a, b ) << std::endl;
+	std::cout << "swap( a, b )"<< std::endl; Mytemplates::swap<int>( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
 }
 
 int	main(void)
