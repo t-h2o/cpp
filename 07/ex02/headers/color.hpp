a2ac545a175d67ff9d425498e455589bb35d6b56
theo <grivel.theo@protonmail.com>
2022-12-10 01:24:37 +0100

    add color

---
+++ 07/ex02/headers/color.hpp
@@ -0,0 +1,15 @@
+#ifndef	COLOR_HPP
+# define COLOR_HPP
+
+# define	COL_RED		"\033[31;1m"
+# define	COL_GRE		"\033[32;1m"
+# define	COL_RES		"\033[0m"
+
+# define	NEWLINE		std::cout << std::endl;
+
+# include	<iostream>
+
+void	title(std::string);
+void	section(std::string);
+
+#endif /* COLOR_HPP */
