0cdd291d664aa2ad7a6fe73140485c4031d69a10
Theo Grivel <tgrivel@c1r9s8.42lausanne.ch>
2022-12-11 16:04:18 +0100

    main: created test
    Signed-off-by: Theo Grivel <tgrivel@c1r9s8.42lausanne.ch>

--- 07/ex02/srcs/main.cpp
+++ 07/ex02/srcs/main.cpp
@@ -2,15 +2,53 @@
 #include	"color.hpp"
 #include	"Arint.hpp"
 
+static void	trycatch(Arint &myarray, unsigned int position, int value)
+{
+	try
+	{
+		myarray[position] = value;
+		std::cout
+			<< COL_GRE
+			<< "Array index["
+			<< position
+			<< "]"
+			<< " = "
+			<< myarray[position]
+			<< COL_RES
+			<< std::endl;
+	}
+	catch (std::exception &e)
+	{
+		print_exception(e, "; size = "
+			+ std::to_string(myarray.size())
+			+ ", position = "
+			+ std::to_string(position));
+	}
+}
+
+static void	test(void)
+{
+	{
+		Arint	arrempty;
+
+		trycatch(arrempty, 0, 42);
+		trycatch(arrempty, 12, 42);
+	}
+	{
+		Arint	arrint(5);
+
+		trycatch(arrint, 4, 42);
+		trycatch(arrint, 5, 42);
+		trycatch(arrint, 0, 42);
+		trycatch(arrint, -1, 42);
+	}
+}
+
 int	main(void)
 {
 	title("Module 07 : Exercice 02 : Array");
 
-	Arint	arrempty;
-	Arint	arrint(5);
-
-	arrint[1] = 42;
-	std::cout << arrint[1] << std::endl;
+	test();
 
 	return 0;
 }
