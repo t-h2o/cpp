52808d8b2288c5311b0cf58d89ef98c6748c9820
theo <grivel.theo@protonmail.com>
2022-12-11 12:53:06 +0100

    Arint: default: init an empty array

--- 07/ex02/srcs/Arint.cpp
+++ 07/ex02/srcs/Arint.cpp
@@ -2,10 +2,12 @@
 
 Arint::Arint(void)
 {
-	message("Arint: default constructor");
+	message("Arint: default constructor (0 element)");
+	this->_myList = new int[0];
 }
 
 Arint::~Arint(void)
 {
 	message("Arint: destructor");
+	delete [] this->_myList;
 }
