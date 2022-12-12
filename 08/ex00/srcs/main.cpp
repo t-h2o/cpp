984f3dcb318873eafb57a4092f007780c1cb1cb4
theo <grivel.theo@protonmail.com>
2022-12-12 09:44:45 +0100

    init a vector with "random" values

--- 08/ex00/srcs/main.cpp
+++ 08/ex00/srcs/main.cpp
@@ -1,9 +1,20 @@
 #include	<iostream>
+#include	<vector>
 #include	"color.hpp"
+
+static void	fill_vec(std::vector<int> &vec)
+{
+	for (int i = 0; i < 10000; i+= 76)
+		vec.push_back((i * i) % 1000);
+}
 
 int	main(void)
 {
 	title("Module 8 : exercice 00 : Easy find");
+
+	std::vector<int>	vec;
+
+	fill_vec(vec);
 
 	return 0;
 }
