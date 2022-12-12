9e1612370de6913af2556c580eaad44980a275c7
Theo Grivel <tgrivel@c1r15s7.42lausanne.ch>
2022-12-12 11:12:42 +0100

    created: easy find
    Signed-off-by: Theo Grivel <tgrivel@c1r15s7.42lausanne.ch>

--- 08/ex00/srcs/main.cpp
+++ 08/ex00/srcs/main.cpp
@@ -8,6 +8,30 @@ static void	fill_vec(std::vector<int> &vec)
 		vec.push_back((i * i) % 1000);
 }
 
+static void	easy_find(std::vector<int> &vec, int number)
+{
+	std::vector<int>::iterator	iter;
+
+	iter = find(vec.begin(), vec.end(), number);
+
+	if (iter != vec.end())
+	{
+		std::cout
+			<< "Easy finds \""
+			<< number
+			<< "\"."
+			<< std::endl;
+	}
+	else
+	{
+		std::cout
+			<< "Easy doesn't find \""
+			<< number
+			<< "\"."
+			<< std::endl;
+	}
+}
+
 int	main(void)
 {
 	title("Module 8 : exercice 00 : Easy find");
@@ -15,6 +39,9 @@ int	main(void)
 	std::vector<int>	vec;
 
 	fill_vec(vec);
+
+	easy_find(vec, 234);
+	easy_find(vec, 344);
 
 	return 0;
 }
