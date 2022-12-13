fd164747d6d73b2cf7c10b139a2e1138c23e0aa8
Theo Grivel <tgrivel@c1r14s7.42lausanne.ch>
2022-12-13 11:16:03 +0100

    Try list container
    Signed-off-by: Theo Grivel <tgrivel@c1r14s7.42lausanne.ch>

--- 08/ex02/srcs/main.cpp
+++ 08/ex02/srcs/main.cpp
@@ -1,9 +1,41 @@
 #include	<iostream>
+#include	<list>
 #include	"color.hpp"
+
+static void	print_int(int number)
+{
+	std::cout << number << std::endl;
+}
+
+static void	test_list(void)
+{
+	section("Try list container");
+
+	std::list<int>	numbers;
+
+	for_each(numbers.begin(), numbers.end(), print_int);
+
+	section("\tPush back 5");
+	numbers.push_back(5);
+	for_each(numbers.begin(), numbers.end(), print_int);
+	
+	section("\tPush back 8");
+	numbers.push_back(8);
+	for_each(numbers.begin(), numbers.end(), print_int);
+	
+	section("\tPush back 10");
+	numbers.push_back(10);
+	for_each(numbers.begin(), numbers.end(), print_int);
+
+	section("\tprint reverse");
+	for_each(numbers.rbegin(), numbers.rend(), print_int);
+}
 
 int	main(void)
 {
 	title("Module 8 : exercice 02 : mstack");
+
+	test_list();
 
 	return 0;
 }
