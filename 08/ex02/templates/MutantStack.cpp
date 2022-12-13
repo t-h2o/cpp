838c66769732e152207548740e887539e6d07442
|°_°| <grivel.theo@protonmail.com>
2022-12-13 22:03:56 +0100

    exercice 2

---
+++ 08/ex02/templates/MutantStack.cpp
@@ -0,0 +1,74 @@
+#include	"MutantStack.hpp"
+
+template <typename T, typename Container>
+MutantStack<T, Container>::MutantStack(void) : std::stack<T, Container>()
+{
+	message("MutantStack: default constructor");
+}
+
+template <typename T, typename Container>
+MutantStack<T, Container>::MutantStack(MutantStack const &other) : std::stack<T, Container>(other)
+{
+	message("MutantStack: copy constructor");
+}
+
+template <typename T, typename Container>
+MutantStack<T, Container>::~MutantStack(void)
+{
+	message("MutantStack: destructor");
+}
+
+template <typename T, typename Container>
+MutantStack<T, Container>	&MutantStack<T, Container>::operator=(MutantStack const &other)
+{
+	message("MutantStack: operator =");
+
+	if (this == &other)
+		return *this;
+
+	this->c = other.c;
+
+	return *this;
+}
+
+template <typename T, typename Container>
+typename Container::iterator MutantStack<T, Container>::begin(void)
+{
+	message("MutantStack: begin");
+	return this->c.begin();
+}
+
+template <typename T, typename Container>
+typename Container::iterator MutantStack<T, Container>::end(void)
+{
+	message("MutantStack: end");
+	return this->c.end();
+}
+
+template <typename T, typename Container>
+typename Container::const_iterator MutantStack<T, Container>::begin(void) const
+{
+	message("MutantStack: begin (const iterator)");
+	return this->c.begin();
+}
+
+template <typename T, typename Container>
+typename Container::const_iterator MutantStack<T, Container>::end(void) const
+{
+	message("MutantStack: end (const iterator)");
+	return this->c.end();
+}
+
+template <typename T, typename Container>
+typename Container::reverse_iterator MutantStack<T, Container>::rbegin(void)
+{
+	message("MutantStack: reverse begin");
+	return this->c.rbegin();
+}
+
+template <typename T, typename Container>
+typename Container::reverse_iterator MutantStack<T, Container>::rend(void)
+{
+	message("MutantStack: reverse end");
+	return this->c.rend();
+}
